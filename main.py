5ae7d78bf80858fce5fbdc506a4573ac13fee23a
Joseph Sebastian Zavala Reyes <sebastian201980s@gmail.com>
2026-07-07 23:02:27 -0500

    HU01: Agregar menú principal

---
+++ main.py
@@ -0,0 +1,9 @@
+def mostrar_menu():
+    print("===== MIRIAM ATELIER =====")
+    print("1. Mostrar catálogo")
+    print("2. Registrar venta")
+    print("3. Mostrar resumen de venta")
+    print("4. Salir")
+
+
+mostrar_menu()
