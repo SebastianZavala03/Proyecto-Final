596492326043acbd1a91c37e1dcf2ce1065b9f41
Joseph Sebastian Zavala Reyes <sebastian201980s@gmail.com>
2026-07-12 00:31:05 -0500

    Datos de los Productos

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
-def mostrar_menu():
-    print("===== MIRIAM ATELIER =====")
-    print("1. Mostrar catálogo")
-    print("2. Registrar venta")
-    print("3. Mostrar resumen de venta")
-    print("4. Salir")
+#Listas
+# Datos de los Productos
+codigosVestidos = [1, 2, 3, 4, 5, 6]
+
+#Datos de tipo cadena
+nombresVestidos = [
+    "Vestido rojo",
+    "Vestido azul",
+    "Vestido verde",
+    "Vestido anaranjado",
+    "Vestido turquesa",
+    "Vestido negro"
+]
+
+#Datos de tipo cadena
+descripcionesVestidos = [
+    "Perfecto para Salir",
+    "Perfecto para la noche",
+    "Perfecto para la playa",
+    "Perfecto para Fiestas",
+    "Perfecto para un dia de otoño",
+    "Perfecto para gala"
+]
+
+#Datos de tipo float
+preciosVestidos = [
+    75.00,
+    80.90,
+    130.00,
+    190.60,
+    65.20,
+    190.50
+]
 
 
-mostrar_menu()
+#Datos de tipo entero
+stockVestidos = [
+    10,
+    8,
+    3,
+    2,
+    20,
+    30
+]
